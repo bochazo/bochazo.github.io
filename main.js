e5600af4a3f99a7f329bd83128dba8356f136f14
Nicolás Escalante <nlante@gmail.com>
2015-01-13 18:16:50 -0300

    angular resources

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
+'use strict';
+
 var Tabletop = require('tableme');
 var url = 'https://docs.google.com/spreadsheets/d/1LkVkb3VFjxBf6JpvTOgklzcwr9OgU_8n8fBpyqhVS4U/pubhtml';
 
 Tabletop.init({
   key: url,
   callback: function(data, tabletop) {
-    console.log(data)
+    console.log(data);
   },
   simpleSheet: false
 });
+
+angular.module('ocampo', ['ngRoute'])
+  .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+
+    $routeProvider
+      .when('/', { controller: 'HomeCtrl', templateUrl: '/site/home.html' })
+      .when('/busqueda', { controller: 'PlaceSearchCtrl', templateUrl: '/place/search.html' })
+      .when('/listado', { controller: 'PlaceListCtrl', templateUrl: '/place/list.html' })
+      .when('/mapa', { controller: 'MapCtrl', templateUrl: '/site/map.html' })
+      .when('/canchas/agregar', { controller: 'PlaceAddCtrl', templateUrl: '/place/add.html' })
+      .when('/canchas/listado/:sport', { controller: 'PlaceListCtrl', templateUrl: '/place/list.html' })
+      .when('/canchas/listado', { controller: 'PlaceListCtrl', templateUrl: '/place/list.html' })
+      .when('/canchas/:id', { controller: 'PlaceDetailCtrl', resolve: {
+        place: ['$route', 'Place', function ($route, Place) {
+          return Place.get($route.current.params).$promise;
+        }]
+      }, templateUrl: '/place/detail.html' })
+      .when('/404', { templateUrl: '/site/404.html' })
+      .otherwise({ templateUrl: '/site/404.html' });
+  }]);
